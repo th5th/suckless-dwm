d7e121e90fdaae417736820852b67ceb1e5ab35a
th5th <ed.willson24@gmail.com>
2013-04-18 20:29:30 +0100

    Changed to a nice reddish hue!

--- config.def.h
+++ config.def.h
@@ -5,10 +5,10 @@ static char font[] = "Inconsolata:pixelsize=14:antialias=true:autohint=true";
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#bbbbbb";
-static const char selbordercolor[]  = "#005577";
-static const char selbgcolor[]      = "#005577";
+static const char selbordercolor[]  = "#660b00";
+static const char selbgcolor[]      = "#660b00";
 static const char selfgcolor[]      = "#eeeeee";
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
